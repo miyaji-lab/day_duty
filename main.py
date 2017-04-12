e9bbf844206686661e1f5cbc2c793c36952ee210
Unknown <shishido.katsunari@gmail.com>
2017-04-13 00:39:32 +0900

    #5
    実行した年，月を取得できるように変更．MemberList.txtで日直メンバーの管理を行う．まだMemberList.txtのファイル出力が必要

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-
+import os
 import sys
 import calendar
 from datetime import date
 from holiday import get_holiday
 
-year = 2017
-month = 5
+members=[]
+year = date.today().year
+month = date.today().month
 holyday_list=[d for d,name in get_holiday(year)]
 
-
 end = calendar.monthrange(year,month)[1]
-member = ["otsuka","shishido","kouno","kodera","qi","nakano","nishida","maezawa","oka"]
+#member = ["otsuka","shishido","kouno","kodera","qi","nakano","nishida","maezawa","oka","komatsu","sugitani","terada","nishiguchi","nishino","hayashi","matsuoka","wang","fu"]
 
+with open("MemberList.txt") as f:
+    for person in f:
+        members.append(person[:-1])
 
-f = open("C:/Users/shishido/Desktop/labduty/foo.txt","w",encoding="utf-8")
+f = open("foo.txt","w")
 
 k = 0
-for i in range(1,end):
-    if date(year,month,i) in holyday_list:
+for day in range(1,end):
+    if date(year,month,day) in holyday_list:
         continue
-    if date(year,month,i).isoweekday() < 6:
-        f.write(date(year,month,i).strftime("%Y/%m/%d") + ": \t" + member[(k)%9] + "\n")
-        k = k + 1
+    if date(year,month,day).isoweekday() < 6:
+        f.write(date(year,month,day).strftime("%Y/%m/%d") + ": \t" + members[k] + "\n")
+        k =( k + 1 ) %len(members)
 
 f.close()
 
-
 a=[]
-for j in range(0,9):
-    a.append(member[(k+j)%9])
+for j in range(len(members)):
+    a.append(members[(k+j)%len(members)])
 
-member = a
-print(member)
+members = a
+print(members)
