c362a1c2355e71904bb55dfeae5b53762f8d63f1
Unknown <shishido.katsunari@gmail.com>
2017-05-02 19:41:07 +0900

    #5
    コードの関数化．テスト未実施．５月分はローカルで実行し，crypto-cybersecにテキストを更新済み

--- main.py
+++ main.py
@@ -7,36 +7,39 @@ from holiday import get_holiday
 def const_members(count,list):
     return list[count:]+list[:count]
 
-members=[]
-year = date.today().year
-month = date.today().month
-holyday_list=[d for d,name in get_holiday(year)]
+def text_gen(f,members):
+    year = date.today().year
+    month = date.today().month
+    end = calendar.monthrange(year,month)[1]
+    holyday_list=[d for d,name in get_holiday(year)]
 
-end = calendar.monthrange(year,month)[1]
-#member = ["otsuka","shishido","kouno","kodera","qi","nakano","nishida","maezawa","oka","komatsu","sugitani","terada","nishiguchi","nishino","hayashi","matsuoka","wang","fu"]
+    k = 0
+    for day in range(1,end):
+        if date(year,month,day) in holyday_list:
+            continue
+        if date(year,month,day).isoweekday() < 6:
+            f.write(date(year,month,day).strftime("%Y/%m/%d") + ": \t" + members[k] + "\n")
+            k =( k + 1 ) %len(members)
+    return f,k
+
+def memberlist_gen(f,members):
+    for i,v in enumerate (members):
+        f.write(v+"\n")
+    return f
+
+
+members=[]
 
 with open("MemberList.txt") as f:
     for person in f:
         members.append(person[:-1])
 
-f = open("foo.txt","w")
-
-k = 0
-for day in range(1,end):
-    if date(year,month,day) in holyday_list:
-        continue
-    if date(year,month,day).isoweekday() < 6:
-        f.write(date(year,month,day).strftime("%Y/%m/%d") + ": \t" + members[k] + "\n")
-        k =( k + 1 ) %len(members)
-
+f,k=text_gen(open("list.txt","w"),members)
 
 f.close()
 
 members=const_members(k,members)
 
-f = open("MemberList1.txt","w")
-
-for i,v in enumerate (members):
-    f.write(v+"\n")
+f = memberlist_gen(open("MemberList.txt","w"),members)
 
 f.close()
